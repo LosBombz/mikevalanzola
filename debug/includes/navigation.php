84c36c65d46b539e7f202b0a3557189445dea9b3
Carlos Escobar <carlos.b.escobar@gmail.com>
2014-01-20 22:41:20 -0500

    includes

---
+++ debug/includes/navigation.php
@@ -0,0 +1,14 @@
+<nav class="main-nav">
+	<ul>
+		<li>
+			<a href="#">Endorsments</a>
+		</li>
+		<li>
+			<a href="#">Contact</a>
+		</li>
+		<li>
+			<a href="#">Contact</a>
+		</li>
+
+	</ul>
+</nav>
