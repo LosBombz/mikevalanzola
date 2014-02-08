403526f1cd05c9c52a3ff8e463169350e7524940
Carlos Escobar <carlos.b.escobar@gmail.com>
2014-02-08 17:34:30 -0500

    working proccess for adding analyics code

--- debug/index.php
+++ debug/index.php
@@ -112,9 +112,9 @@
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', <%= ua%>, 'mikevalanzola.com');
+	  ga('create', '<%= ua%>', 'mikevalanzola.com');
 	  ga('send', 'pageview');
 	</script>
-	-->
+	 /build -->
 </body>
 </html>
