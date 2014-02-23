d669ede94c01cda2f03696d0a03a039469b59d7c
Carlos Escobar <carlos.b.escobar@gmail.com>
2014-02-23 17:41:34 -0500

    added contact page

---
+++ debug/contact.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en-US"> 
+<head>
+	<title>Michael Valanzola Massachusetts State Senate | Contact information</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="">
+	<link rel="shortcut icon" href="favicon.ico">
+
+	
+	<!-- Unicode CSS Loader: place this in the head of your page -->
+	<script>
+		/* grunticon Stylesheet Loader | https://github.com/filamentgroup/grunticon | (c) 2012 Scott Jehl, Filament Group, Inc. | MIT license. */
+		window.grunticon=function(e){if(e&&3===e.length){var t=window,n=!!t.document.createElementNS&&!!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect&&!!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1"),A=function(A){var o=t.document.createElement("link"),r=t.document.getElementsByTagName("script")[0];o.rel="stylesheet",o.href=e[A&&n?0:A?1:2],r.parentNode.insertBefore(o,r)},o=new t.Image;o.onerror=function(){A(!1)},o.onload=function(){A(1===o.width&&1===o.height)},o.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};
+		grunticon( [ "styles/css/icons.data.svg.css", "styles/css/icons.data.png.css", "styles/css/icons.fallback.css" ] );
+	</script>
+	
+	<noscript><link href="styles/css/icons.fallback.css" rel="stylesheet"></noscript>
+	<link rel="stylesheet" media="screen" href="styles/css/main.css">
+	<script src="scripts/libs/modernizr.js"></script>
+
+	<!--[if lt IE 9]>
+
+	<![endif]-->
+
+
+	<!--[if lt IE 8]>
+
+	<![endif]-->
+	<!--[if lt IE 7]> <![endif]-->
+
+</head>
+<body>
+	<?php include("includes/header.php"); ?>
+	<?php include("includes/navigation.php"); ?>
+	
+	<div data-module="res-slider-module" class="slider-container">
+		<ul class="slider">
+			<li>
+				<img src="images/content/districtMap(banner).jpg" alt="Mike Valanzola in front of the Wales Town Hall" />
+			</li>
+		</ul>
+	</div>
+	
+
+	<h1>1st Worcester Hampden Hampshire Middlesex district</h1>
+
+	<div class="district-container">
+		
+		<div class="content-container">
+			<div class="district">
+				<div class="column left">
+					<h2>Worcester County</h2>
+					<ul>
+						<li>Ashburnham</li>
+						<li>Athol</li>
+						<li>Barre</li>
+						<li>Brookfield</li>
+						<li>Charlton</li>
+						<li>East Brookfield</li>
+						<li>Hardwick</li>
+						<li>Hubbardston</li>
+						<li>New Braintree</li>
+						<li>North Brookfield</li>
+						<li>Oakham</li>
+						<li>Paxton</li>
+						<li>Petersham</li>
+						<li>Phillipston</li>
+						<li>Rutland</li>
+						<li>Spencer</li>
+						<li>Sturbridge</li>
+						<li>Templeton</li>
+						<li>West Brookfield</li>
+						<li>Warren</li>
+						<li>Winchendon</li>
+					</ul>
+
+					<h2>Hampden County</h2>
+
+					<ul>
+						<li>Brimfield</li>
+						<li>Holland</li>
+						<li>Monson</li>
+						<li>Palmer</li>
+						<li>Wales</li>
+					</ul>
+
+					<h2>Hampshire County</h2>
+					<ul>
+						<li>Ware</li>
+					</ul>
+
+
+					<h2>Middlesex County</h2>
+					<ul>
+						<li>Ashby</li>
+					</ul>
+				</div>
+				<div class="column right">
+					<img src="images/content/districtMap.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<?php include("includes/footer.php"); ?>
+	
+	<!-- build:js scripts/main.js -->
+	<script data-main="scripts/app.main.js" src="scripts/components/requirejs/require.js"></script>
+	<script src="scripts/require.config.js"></script>
+	<!-- /build -->
+	<!-- build:template
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', '<%= ua%>', 'mikevalanzola.com');
+	  ga('send', 'pageview');
+	</script>
+	 /build -->
+</body>
+</html>
