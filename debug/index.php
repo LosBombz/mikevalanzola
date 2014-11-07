89bd648818b858d9c9148c362ef7fac1e3643886
Carlos Escobar <carlos.b.escobar@gmail.com>
2014-11-07 08:59:23 -0500

    copy

--- debug/index.php
+++ debug/index.php
@@ -39,7 +39,7 @@
 		<div class="content-container">
 			<div class="bio">
 			<img src="images/content/mike-headshot.jpg" alt="Mike Valanzola headshot" class="head-shot">
-			<p>The last ten months have been an incredible journey! I'm grateful for all the support I received and the friendships I've created. This is an amazing  <br />district filled with incredible people. <br /><br />I wish our new Senator well as she moves into the role and will continue to work with all our elected officials for the betterment of our commonwealth!<br /> Thank you for your support! <br /><br />&ndash; Mike Valanzola </p>
+			<p>The last ten months have been an incredible journey! I'm grateful for all the support I received and the friendships I've created. This is an amazing district filled with fantastic people. <br /><br />I wish our new Senator well as she moves into the role and will continue to work with all our elected officials for the betterment of our commonwealth!<br /> Thank you for your support! <br /><br />&ndash; Mike Valanzola </p>
 			</div>
 		</div>
 	</div>
